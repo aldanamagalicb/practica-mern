c70c45f0784543efa0ec29382a93028a87cefd99
unknown <aldanamagali25@gmail.com>
2022-11-01 17:17:43 -0300

    creacion de la app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Soy una card
+          Soy una card jiji
         </p>
         <a
           className="App-link"
